0ed0412b2509aee01f77a77b44a6de601cb47f03
Venkatesh <venkateshbelgaonkar777@gmail.com>
2022-08-09 17:29:26 +0530

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String name = "Venkatesh";
+        int number = 5;
+        boolean value = true;
+        char ch = 'a';
+        System.out.println(name+" "+number+" "+value+" "+ch);
+    }
+}
